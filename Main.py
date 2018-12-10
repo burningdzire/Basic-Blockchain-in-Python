4939b4000b098f64ce7127c42d58aec57d67266f
SharmaMohit <mohit.sharma@tatahealth.com>
2018-12-10 17:55:50 +0530

    Basic blockchain structure

--- Main.py
+++ Main.py
@@ -1,7 +1,18 @@
 from HashGenerator import HashGenerator
 from BlockChain import BlockChain
 
+def printBlock(newBlock):
+    print("index: ", newBlock.index)
+    print("created: ", newBlock.created)
+    print("transaction Sender: ", newBlock.transaction.getSender())
+    print("transaction Receiver : ", newBlock.transaction.getReceiver())
+    print("transaction Amount : ", newBlock.transaction.getAmount())
+    print("currentHash: ", newBlock.currentHash)
+    print("previousHash: ", newBlock.previousHash, "\n")
+
 bc = BlockChain()
+printBlock(bc.getCurrentBlock())
 bc.addNewBlock("Ben", "Gwyen", 225)
-temp = bc.chain[-1].transaction.getReceiver()
-print(len(bc.chain))
+printBlock(bc.getCurrentBlock())
+bc.addNewBlock("Hello", "Delhi", 1050)
+printBlock(bc.getCurrentBlock())
